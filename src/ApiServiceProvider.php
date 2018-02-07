e44e689aa9dd3c968dfd6dac5d52ced6c9255a83
DavidNineRoc <1033404553@qq.com>
2018-02-07 14:59:15 +0800

    :sparkles: 可以使用命令创建API控制器

--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace DavidNineRoc\ApiHelper;
 
+use DavidNineRoc\ApiHelper\Commands\MakeApiController;
 use Illuminate\Support\ServiceProvider;
 
 class ApiServiceProvider extends ServiceProvider
@@ -15,9 +16,16 @@ class ApiServiceProvider extends ServiceProvider
     {
         // 生成控制器
         $this->publishes([
-            __DIR__ . '/Controllers/Api/ApiController.tpl' => app_path('Http//Controllers/Api/ApiController.php'),
+            __DIR__ . '/Controllers/ApiController.tpl' => app_path('Http//Controllers/Api/ApiController.php'),
             __DIR__ . '/Services/ResponseServe.tpl' => app_path('Http/Services/ResponseServe.php')
         ]);
+
+        // 绑定控制器生成命令
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeApiController::class
+            ]);
+        }
     }
 
     /**
---
+++ src/Controllers/Controller.tpl
@@ -0,0 +1,11 @@
+<?php
+
+namespace DummyNamespace;
+
+use Illuminate\Http\Request;
+use DummyRootNamespaceHttp\Controllers\Api\ApiController;
+
+class DummyClass extends ApiController
+{
+    //
+}
